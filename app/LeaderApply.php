88df180ce1ab7b9de03a5971bb2297d9cbcdad44
Tiege <wudongbo@youxiake.com>
2016-05-26 17:05:50 +0800

    add model

---
+++ app/LeaderApply.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeaderApply extends Model
+{
+
+    protected $table = 'yxk_leader_apply';
+
+}
---
+++ app/LeaderApplyday.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeaderApplyday extends Model
+{
+
+    protected $table = 'yxk_leader_applyday';
+
+}
